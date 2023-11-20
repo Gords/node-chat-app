bbe9039b21f22e1e08770c19fe79dda3726eaedf
Fernando <gordsftrl@gmail.com>
2023-11-21 01:21:06 +0900

    added aditional methods to get user and get user in a specific room

--- src/utils/users.js
+++ src/utils/users.js
@@ -1,6 +1,5 @@
 const users = []
 
-// addUser, removeUser, getUser, getUsersInRoom
 
 const addUser = ({ id, username, room }) => {
     // Clean the data
@@ -40,14 +39,18 @@ const removeUser = (id) => {
     }
 }
 
-addUser({
-    id: 22,
-    username: 'Andrew',
-    room: 'South Philly'
-})
+const getUser = (id) => {
+    return users.find((user)=> user.id === id)
+}
 
-console.log(users)
+const getUsersInRoom = (room) => {
+    room = room.trim().toLowerCase()
+    return users.filter((user)=> user.room === room)
+}
 
-const removedUser = removeUser(22)
-console.log(removedUser)
-console.log(users)
+module.exports = {
+    addUser,
+    removeUser,
+    getUser,
+    getUsersInRoom
+}
